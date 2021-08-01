e7f3102a8cf7651de744d4f7adfdae35bf4c1d9e
murat sarıbas <muratsaribas97@gmail.com>
2021-08-01 15:59:40 +0300

    New files added

---
+++ exceptions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try{
+                Console.WriteLine("Bir sayı giriniz: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sayı: " + num1);
+
+            }
+            catch(Exception ex){
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }
+            finally{
+                Console.WriteLine("İşlem tamam");
+                
+            }
+
+            try{
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-99999999999999");
+            }
+            catch(ArgumentNullException ex){
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }
+            catch(FormatException ex){
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }
+            catch(OverflowException ex){
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }
+            finally{
+                Console.WriteLine("İşlem tamam");
+            }
+        }
+    }
+}
--- type_conversion/Program.cs
+++ type_conversion/Program.cs
@@ -6,7 +6,62 @@ namespace type_conversion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // implicit
+
+            byte a = 5;
+            sbyte sb = 10;
+            short c = 30;
+
+            int d = a+sb+c;
+            Console.WriteLine("d: " + d);
+
+            long h = d;
+            Console.WriteLine("h: "+h);
+
+            float i = h;
+            Console.WriteLine("i: " + i);
+
+            string s = "test";
+            char ch = 'k';
+            object g = e+s+d;
+            Console.WriteLine("g: " + g);
+
+
+            // explicit
+
+            int x = 3;
+            byte y = (byte) x;
+            Console.WriteLine("y: "+y);
+
+            int z = 100;
+            byte t = (byte) z;
+            Console.WriteLine("t: "+t);
+
+            float w = 10.3f;
+            byte v = (byte)w;
+            Console.WriteLine("v: " + v);
+
+            // toString
+            int xx = 4;
+            string yy = xx.ToString();
+            Console.WriteLine("yy: "+ yy);
+
+            yy = 12.3f.ToString();
+            Console.WriteLine("yy: "+yy);
+
+            // System.Convert
+
+            string s1 ="10", s2="20";
+            int sayı1, sayı2;
+            int toplam;
+
+            sayı1 = Convert.ToInt32(s1);
+            sayı2 = Convert.ToInt32(s2);
+            toplam = sayı1 + sayı2;
+            Console.WriteLine("t: " + toplam);
+
+
+
         }
     }
 }
