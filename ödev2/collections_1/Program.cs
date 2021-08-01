907320ffd4bad194611cff5ad344eabc691dac78
murat sarıbas <muratsaribas97@gmail.com>
2021-08-01 23:38:09 +0300

    New files added

---
+++ ödev2/collections_1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/* 
+    Klavyeden girilen 20 adet pozitif sayının asal ve 
+    asal olmayan olarak 2 ayrı listeye atın. (ArrayList sınıfını kullanara yazınız.)
+    -Negatif ve numeric olmayan girişleri engelleyin.
+    -Her bir dizinin elemanlarını büyükten küçüğe olacak şekilde ekrana yazdırın.
+    -Her iki dizinin eleman sayısını ve ortalamasını ekrana yazdırın.
+
+*/
+
+
+namespace collections_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList prime = new ArrayList();
+            ArrayList notPrime = new ArrayList();
+            int count = 0;
+            string input;
+            int number;
+            while(count < 20){
+                Console.WriteLine((count+1).ToString()+ "."+" sayı: ");
+                input = Console.ReadLine();
+                if(Int32.TryParse(input, out number)){
+                    if(number < 0)
+                        Console.WriteLine("Pozitif sayı girin!");
+                    else{
+                        count++;
+                        if(isPrime(number))
+                            prime.Add(number);
+                        else
+                            notPrime.Add(number);
+                    }
+                }
+                else
+                    Console.WriteLine("Hatalı giriş!");
+            }
+
+            prime.Sort();
+            prime.Reverse();
+            Console.WriteLine("Prime numbers: ");
+            foreach(int item in prime)
+                Console.WriteLine(item);
+            
+            notPrime.Sort();
+            notPrime.Reverse();
+            Console.WriteLine("Not Prime numbers: ");
+            foreach(int item in notPrime)
+                Console.WriteLine(item);
+
+            int sum = 0;
+            foreach (int item in prime)
+                sum += item;
+            Console.WriteLine("Prime numbers size: " + prime.Count +" Mean: "+ sum/prime.Count);
+
+            sum = 0;
+            foreach (int item in notPrime)
+                sum += item;
+            Console.WriteLine("Prime numbers size: " + notPrime.Count +" Mean: "+ sum/notPrime.Count);
+
+            
+
+
+
+        }
+
+        public static bool isPrime(int n){
+            int m = n/2;
+            bool flag = true;
+            for(int i=2; i<m+1; i++){
+                if(n % i == 0){
+                    flag = false;
+                    break;
+                }
+            }
+            return flag;
+
+        }
+    }
+}
---
+++ ödev2/collections_2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+/*
+    Klavyeden girilen 20 adet sayının en büyük 3 tanesi ve en küçük 3 tanesi bulan, 
+    her iki grubun kendi içerisinde ortalamalarını alan ve bu ortalamaları ve 
+    ortalama toplamlarını console'a yazdıran programı yazınız. 
+    (Array sınıfını kullanarak yazınız.)
+*/
+
+
+namespace collections_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[20];
+            int[] max = new int[3];
+            int[] min = new int[3];
+            int count = 0;
+            string input;
+            int number;
+            while(count < 20){
+                Console.WriteLine((count+1).ToString()+ "."+" sayı: ");
+                input = Console.ReadLine();
+                if(Int32.TryParse(input, out number)){
+                    arr[count] = number;
+                    count++;
+                }
+                else
+                    Console.WriteLine("Hatalı giriş!");
+            }
+            Array.Sort(arr);
+            min[0] = arr[0];
+            min[1] = arr[1];
+            min[2] = arr[2];
+
+            max[0] = arr[arr.Length -1];
+            max[1] = arr[arr.Length -2];
+            max[2] = arr[arr.Length -3];
+
+            for(int i=0; i<3; i++)
+                Console.WriteLine("Min: " + min[i] + "  Max: " + max[i]);
+            double minOrt = (min[0]+min[1]+min[2])/3;
+            double maxOrt = (max[0]+max[1]+max[2])/3;
+            Console.WriteLine("Min arr ort: " + minOrt);
+            Console.WriteLine("Max arr ort: " + maxOrt);
+            Console.WriteLine("Ort: " + (minOrt+maxOrt)/2);
+
+        }
+    }
+}
