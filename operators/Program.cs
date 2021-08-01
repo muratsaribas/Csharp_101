003af47129e6c847cf4a5de0acc714372cfd41d8
murat sarıbas <muratsaribas97@gmail.com>
2021-08-01 14:56:58 +0300

    New files added

---
+++ operators/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 2;
+            int y = 3;
+            y = y+2;
+
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y /= 2;
+            Console.WriteLine(y);
+            x *= 2;
+            Console.WriteLine(x);
+
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if(isSuccess && isCompleted)
+                Console.WriteLine("Perfect");
+            if(isSuccess || isCompleted)
+                Console.WriteLine("Great");
+            if(isSuccess && !isCompleted)
+                Console.WriteLine("Fine");
+
+            int a = 3;
+            int b = 3;
+            bool result = a < b;
+
+            Console.WriteLine(result);
+            result = a > b;
+            Console.WriteLine(result);
+            result = a <= b;
+            Console.WriteLine(result);
+            result = a >= b;
+            Console.WriteLine(result);
+            result = a == b;
+            Console.WriteLine(result);
+            result = a != b;
+            Console.WriteLine(result);
+
+        }
+    }
+}
